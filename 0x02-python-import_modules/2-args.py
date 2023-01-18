4b6029712c5f11d46d5bd8ea707d174a506fae82
Emmanuel Saura <emmanuelsaura007@gmail.com>
2023-01-18 02:06:59 -0800

    my commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     else:
         i = 1
         if (leng == 2):
-            print("{} argument".format(leng - 1))
+            print("{} argument:".format(leng - 1))
         else:
-            print("{} arguments".format(leng - 1))
+            print("{} arguments:".format(leng - 1))
         while (i <= (leng - 1)):
             print("{}: {}".format(i, sys.argv[i]))
             i = i + 1
