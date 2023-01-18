0d90da7a8fe480397c76e140e49836b46237a7c9
Emmanuel Saura <emmanuelsaura007@gmail.com>
2023-01-18 02:02:28 -0800

    my commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    leng = len(sys.argv)
+    if (leng <= 1):
+        print("0 arguments.")
+    else:
+        i = 1
+        if (leng == 2):
+            print("{} argument".format(leng - 1))
+        else:
+            print("{} arguments".format(leng - 1))
+        while (i <= (leng - 1)):
+            print("{}: {}".format(i, sys.argv[i]))
+            i = i + 1
