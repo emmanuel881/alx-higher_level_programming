0dc0e1e9ee1b578ce72283640204c27ee1f1c9e7
Emmanuel Saura <emmanuelsaura007@gmail.com>
2023-01-22 23:06:49 -0800

    my commit

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    leng = len(my_list)
+    count = 1
+    while(leng > 0):
+        print("{}".format(my_list[(count * -1)]))
+        leng = leng - 1
+        count += 1
