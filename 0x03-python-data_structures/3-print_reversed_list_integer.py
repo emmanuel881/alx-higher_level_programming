3470c132e269c83984664d6cb14f33f2083b4f3d
Emmanuel Saura <emmanuelsaura007@gmail.com>
2023-01-22 23:10:28 -0800

    my commit

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,6 @@ def print_reversed_list_integer(my_list=[]):
     leng = len(my_list)
     count = 1
     while(leng > 0):
-        print("{}".format(my_list[(count * -1)]))
+        print("{:d}".format(my_list[(count * -1)]))
         leng = leng - 1
         count += 1
